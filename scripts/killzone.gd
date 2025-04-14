2ce06f7af621464ec0963dee970933bca9eafca1
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2025-04-14 16:49:57 -0700

    Slow down game upon death

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -14,8 +14,11 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died.")
+	body.get_node("CollisionShape2D").queue_free()
+	Engine.time_scale = 0.5
 	timer.start()
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
